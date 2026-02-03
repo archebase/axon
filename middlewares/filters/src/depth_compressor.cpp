bd91531a3ec4fdee71213f91abf84f5f7d692d2f
Liangwei XU <xlwyes@gmail.com>
2026-02-03 17:04:52 +0800

    refactor(filters): simplify compression logic and remove unused parameters

--- middlewares/filters/src/depth_compressor.cpp
+++ middlewares/filters/src/depth_compressor.cpp
@@ -19,46 +19,36 @@ bool DepthCompressor::compress(
 
   // Cast to int16_t (16UC1 encoding)
   const int16_t* depth_data_i16 = reinterpret_cast<const int16_t*>(depth_data);
-  int num_pixels = static_cast<int>(width * height);
 
-  // Compress using dlz library
-  // Note: dlz::CompressEx takes non-const pointer but doesn't modify input (verified by inspection)
-  // The const_cast is safe here - dlz only reads from the input buffer
+  // Use CompressToFile to include DLZF header with width/height information
+  // This is necessary because CompressedImage message doesn't contain image dimensions
+  // Note: dlz::CompressToFile takes non-const pointer but doesn't modify input (verified by
+  // inspection) The const_cast is safe here - dlz only reads from the input buffer
   std::vector<char> compressed_buffer;
-  size_t compressed_size;
 
-  bool success = dlz::CompressEx(
+  bool success = dlz::CompressToFile(
     const_cast<int16_t*>(depth_data_i16),
-    num_pixels,
+    static_cast<uint32_t>(width),
+    static_cast<uint32_t>(height),
     compressed_buffer,
-    0,
-    compressed_size,
     static_cast<dlz::CompressionLevel>(config_.level),
-    static_cast<dlz::Threading>(config_.threading),
-    config_.num_threads
+    static_cast<dlz::Threading>(config_.threading)
   );
 
   if (!success) {
     return false;
   }
 
-  // Copy to output vector
-  compressed_data.assign(compressed_buffer.begin(), compressed_buffer.begin() + compressed_size);
+  // Copy to output vector (includes DLZF header with magic, width, height, level)
+  compressed_data.assign(compressed_buffer.begin(), compressed_buffer.end());
 
   return true;
 }
 
 std::string DepthCompressor::get_compression_format() const {
-  switch (config_.level) {
-    case CompressionLevel::kFast:
-      return "dlz_fast";
-    case CompressionLevel::kMedium:
-      return "dlz_medium";
-    case CompressionLevel::kMax:
-      return "dlz_max";
-    default:
-      return "dlz_medium";
-  }
+  // DLZF file format: [magic(4)][width(4)][height(4)][level(4)][compressed data...]
+  // The format is always "dlzf" since the compression level is stored in the header
+  return "dlzf";
 }
 
 void DepthCompressor::set_config(const Config& config) {
